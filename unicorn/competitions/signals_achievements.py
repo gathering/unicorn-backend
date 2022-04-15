7658ccd49d1b544aea084d6cfac3e1599a7bd0d6
Erik Hetland <erik@zoodo.no>
2022-04-15 19:46:12 +0200

    screw this signal

--- unicorn/competitions/signals_achievements.py
+++ unicorn/competitions/signals_achievements.py
@@ -2,7 +2,7 @@ import requests
 from accounts.constants import USER_ROLE_CREW, USER_ROLE_PARTICIPANT
 from accounts.models import User
 from competitions.constants import ENTRY_STATUS_QUALIFIED
-from competitions.models import Entry, Vote
+from competitions.models import Entry
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -26,33 +26,33 @@ def get_social_user(user: User):
     return (social, social_type)
 
 
-@receiver(post_save, sender=Vote)
-def user_has_voted(instance: Vote, created, **kwargs):
-    # do not process if we don't have a webhook destination
-    if not settings.ACHIEVEMENTS_WEBHOOK:
-        return
+# @receiver(post_save, sender=Vote)
+# def user_has_voted(instance: Vote, created, **kwargs):
+#     # do not process if we don't have a webhook destination
+#     if not settings.ACHIEVEMENTS_WEBHOOK:
+#         return
 
-    # only run for first vote per entry
-    if not created:
-        return
+#     # only run for first vote per entry
+#     if not created:
+#         return
 
-    try:
-        # find correct social user
-        social, social_type = get_social_user(instance.user)
+#     try:
+#         # find correct social user
+#         social, social_type = get_social_user(instance.user)
 
-        # build payload
-        payload = {
-            "type": "user_has_voted",
-            "user": social.extra_data["sub"],
-            "user_type": social_type,
-            "entry": instance.entry.id,
-            "competition": instance.entry.competition.id,
-        }
+#         # build payload
+#         payload = {
+#             "type": "user_has_voted",
+#             "user": social.extra_data["sub"],
+#             "user_type": social_type,
+#             "entry": instance.entry.id,
+#             "competition": instance.entry.competition.id,
+#         }
 
-        # send event to achievements
-        requests.post(settings.ACHIEVEMENTS_WEBHOOK, json=payload)
-    except KeyError:
-        return
+#         # send event to achievements
+#         requests.post(settings.ACHIEVEMENTS_WEBHOOK, json=payload)
+#     except KeyError:
+#         return
 
 
 @receiver(post_save, sender=Entry)
