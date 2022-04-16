adfeae7525b3d4ac2fb61ee1f0b302e6c42adfe9
Erik Hetland <erik@zoodo.no>
2022-04-16 12:14:57 +0200

    Trying to fix achievement signals with better code

--- unicorn/competitions/signals_achievements.py
+++ unicorn/competitions/signals_achievements.py
@@ -2,7 +2,7 @@ import requests
 from accounts.constants import USER_ROLE_CREW, USER_ROLE_PARTICIPANT
 from accounts.models import User
 from competitions.constants import ENTRY_STATUS_QUALIFIED
-from competitions.models import Entry
+from competitions.models import Entry, Vote
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -21,38 +21,37 @@ def get_social_user(user: User):
         social_type = "participant"
 
     else:
-        return None
+        return (None, None)
 
     return (social, social_type)
 
 
-# @receiver(post_save, sender=Vote)
-# def user_has_voted(instance: Vote, created, **kwargs):
-#     # do not process if we don't have a webhook destination
-#     if not settings.ACHIEVEMENTS_WEBHOOK:
-#         return
+@receiver(post_save, sender=Vote)
+def user_has_voted(instance: Vote, created, **kwargs):
+    # do not process if we don't have a webhook destination
+    if not settings.ACHIEVEMENTS_WEBHOOK:
+        return
 
-#     # only run for first vote per entry
-#     if not created:
-#         return
+    # only run for first vote per entry
+    if not created:
+        return
 
-#     try:
-#         # find correct social user
-#         social, social_type = get_social_user(instance.user)
+    # find correct social user
+    social, social_type = get_social_user(instance.user)
+    if not social:
+        return
 
-#         # build payload
-#         payload = {
-#             "type": "user_has_voted",
-#             "user": social.extra_data["sub"],
-#             "user_type": social_type,
-#             "entry": instance.entry.id,
-#             "competition": instance.entry.competition.id,
-#         }
+    # build payload
+    payload = {
+        "type": "user_has_voted",
+        "user": social.extra_data.get("sub", ""),
+        "user_type": social_type or "",
+        "entry": instance.entry.id,
+        "competition": instance.entry.competition.id,
+    }
 
-#         # send event to achievements
-#         requests.post(settings.ACHIEVEMENTS_WEBHOOK, json=payload)
-#     except KeyError:
-#         return
+    # send event to achievements
+    requests.post(settings.ACHIEVEMENTS_WEBHOOK, json=payload)
 
 
 @receiver(post_save, sender=Entry)
@@ -68,12 +67,14 @@ def user_got_qualified_entry(instance: Entry, **kwargs):
     for user in instance.contributors.all():
         # find correct social user
         social, social_type = get_social_user(user)
+        if not social:
+            return
 
         # build payload
         payload = {
             "type": "user_got_qualified_entry",
-            "user": social.extra_data["sub"],
-            "user_type": social_type,
+            "user": social.extra_data.get("sub", ""),
+            "user_type": social_type or "",
             "entry": instance.id,
             "competition": instance.competition.id,
         }
