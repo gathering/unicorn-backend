c383ccfc21e0f155b28f6362960d79c4cf8c445e
Erik Hetland <erik@erikhetland.no>
2023-02-11 11:19:40 +0100

    Allow compoadmin groups to add new competitions

--- unicorn/competitions/apps.py
+++ unicorn/competitions/apps.py
@@ -2,19 +2,26 @@ from django.apps import AppConfig
 
 
 def create_permission_groups(**kwargs):
-    from django.contrib.auth.models import Group
+    from django.contrib.auth.models import Group, Permission
+
     from .constants import GENRE_CATEGORY_CHOICES
 
+    p = Permission.objects.get(
+        content_type__app_label="competitions", codename="add_competition"
+    )
     for category in GENRE_CATEGORY_CHOICES:
-        Group.objects.get_or_create(name="p-compoadmin-{}".format(str(category[0])))
+        g = Group.objects.get_or_create(name="p-compoadmin-{}".format(str(category[0])))
+        if g[1]:
+            # add permission if the group was created
+            g[0].permissions.add(p)
 
 
 class CompetitionsConfig(AppConfig):
     name = "competitions"
 
     def ready(self):
-        from django.db.models.signals import post_migrate
         import competitions.signals  # noqa: F401
         import competitions.signals_achievements  # noqa: F401
+        from django.db.models.signals import post_migrate
 
         post_migrate.connect(create_permission_groups, sender=self)
