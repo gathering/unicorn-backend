9a9d26d492d322c87986bc9e90f3e1c21a9b5488
Erik Hetland <erik@erikhetland.no>
2025-04-19 13:40:35 +0200

    fix: tweak api metadata generation
    skip checking permissions on views when determining actions. this makes sure that users can options their way to info without having permissions to do these actions on all similar objects

--- unicorn/unicorn/api.py
+++ unicorn/unicorn/api.py
@@ -1,9 +1,12 @@
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
-from rest_framework import serializers
+from rest_framework import exceptions, serializers
 from rest_framework.exceptions import APIException
 from rest_framework.metadata import SimpleMetadata
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.renderers import BaseRenderer, BrowsableAPIRenderer
+from rest_framework.request import clone_request
 from rest_framework.utils.field_mapping import ClassLookupDict
 
 #
@@ -133,6 +136,33 @@ class ExtendedMetadata(SimpleMetadata):
             serializers.JSONField: "json",
         }
     )
+
+    def determine_actions(self, request, view):
+        """
+        For generic class based views we return information about
+        the fields that are accepted for 'PUT' and 'POST' methods.
+        """
+        actions = {}
+        for method in {"PUT", "POST"} & set(view.allowed_methods):
+            view.request = clone_request(request, method)
+            try:
+                # Test global permissions
+                # if hasattr(view, "check_permissions"):
+                #     view.check_permissions(view.request)
+                # Test object permissions
+                if method == "PUT" and hasattr(view, "get_object"):
+                    view.get_object()
+            except (exceptions.APIException, PermissionDenied, Http404):
+                pass
+            else:
+                # If user has appropriate permissions for the view, include
+                # appropriate metadata about the fields that should be supplied.
+                serializer = view.get_serializer()
+                actions[method] = self.get_serializer_info(serializer)
+            finally:
+                view.request = request
+
+        return actions
 
 
 #
